7dc4da0533a33062510f95c083653f90cd22cf71
Oksanaka848 <okmitka@mail.ru>
2022-11-30 22:06:47 +0300

    ch

--- card.js
+++ card.js
@@ -1,107 +1,134 @@
-const batman = [
-  {
-    name: "Бэтмен",
-    univers: "DC Comics",
-    alterago: "Брюс Уэйн",
-    job: "борец с преступностью, филантроп, миллиардер",
-    friens: "Робин, Бэтгерл",
-    powers: "интеллект, обширные познания, знания боевых искусств, ловкость",
-  },
-];
+let superherous = `[{
 
-const superman = [
   {
-    name: "Супермен",
-    univers: "DC Comics",
-    alterago: "Кларк Кент",
-    job: "борец за справедливость",
-    friens: "собака Крипто",
-    powers:
+    "name": "Бэтмен",
+    "univers": "DC Comics",
+    "alterago": "Брюс Уэйн",
+    "job": "борец с преступностью, филантроп, миллиардер",
+    "friens": "Робин, Бэтгерл",
+    "powers": "интеллект, обширные познания, знания боевых искусств, ловкость",
+  },
+
+  {
+    "name": "Супермен",
+    "univers": "DC Comics",
+    "alterago": "Кларк Кент",
+    "job": "борец за справедливость",
+    "friens": "собака Крипто",
+    "powers":
       "непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм",
   },
-];
-const ironman = [
+
   {
-    name: "Железный человек",
-    univers: "Marvel Comics",
-    alterago: "Тони Старк",
-    job: "гений, миллиардер, плейбой, филантроп",
-    friens: "Мстители",
-    powers:
+    "name": "Железный человек",
+    "univers": "Marvel Comics",
+    "alterago": "Тони Старк",
+    "job": "гений, миллиардер, плейбой, филантроп",
+    "friens": "Мстители",
+    "powers":
       "высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы",
   },
-];
-const spiderman = [
+
+
   {
-    name: "Спайдер-мен/Человек-паук",
-    univers: "Marvel Comics",
-    alterago: "Питер Паркер",
-    job: "борец за справедливость, студент, фотограф",
-    friens: "Мстители, Фантастическая четверка, Люди Икс",
-    powers:
+    "name": "Спайдер-мен/Человек-паук",
+    "univers": "Marvel Comics",
+    "alterago": "Питер Паркер",
+    "job": "борец за справедливость, студент, фотограф",
+    "friens": "Мстители, Фантастическая четверка, Люди Икс",
+    "powers":
       "сверхчеловеческие рефлексы, «паучье чутье», способность прилепляться к твердым поверхностям, производство паутины",
   },
-];
-const captainAmerica = [
+
+
   {
-    name: "Капитан Америка",
-    univers: "Marvel Comics",
-    alterago: "Стивен Роджерс",
-    job: "супер-солдат",
-    friens: "Мстители",
-    powers:
+    "name": "Капитан Америка",
+    "univers": "Marvel Comics",
+    "alterago": "Стивен Роджерс",
+    "job": "супер-солдат",
+    "friens": "Мстители",
+    "powers":
       "сила, выносливость, бессмертие, быстрая регенерация, мастерство скрытности и боя",
   },
-];
-const Tor = [
+
+
   {
-    name: "Тор",
-    univers: "Marvel Comics",
-    alterago: "нет; полное имя — Тор Одинсон",
-    job: "борец за справедливость, скандинавский бог",
-    friens: "Мстители",
-    powers: "все, что может бог, плюс молот Мьелнир",
+    "name": "Тор",
+    "univers": "Marvel Comics",
+    "alterago": "нет; полное имя — Тор Одинсон",
+    "job": "борец за справедливость, скандинавский бог",
+    "friens": "Мстители",
+    "powers": "все, что может бог, плюс молот Мьелнир",
   },
-];
-const halk = [
+
+
   {
-    name: "Халк",
-    univers: "Marvel Comics",
-    alterago: "Брюс Беннер",
-    job: "супергерой, борец за справедливость, ученый-биохимик",
-    friens: "Мстители",
-    powers: "сверхчеловеческая сила искорость, выносливость, полет",
+    "name": "Халк",
+    "univers": "Marvel Comics",
+    "alterago": "Брюс Беннер",
+    "job": "супергерой, борец за справедливость, ученый-биохимик",
+    "friens": "Мстители",
+    "powers": "сверхчеловеческая сила искорость, выносливость, полет",
   },
-];
-const wonderWoman = [
+
+
   {
-    name: "Чудо-женщина",
-    univers: "DC Comics",
-    alterago: "Диана Принс",
-    job: "супергероиня, секретарь-референт",
-    friens: "Лига Справедливости, Бэтмен, Супермен",
-    powers: "верхчеловеческая сила искорость, выносливость, полет",
+    "name": "Чудо-женщина",
+    "univers": "DC Comics",
+    "alterago": "Диана Принс",
+    "job": "супергероиня, секретарь-референт",
+    "friens": "Лига Справедливости, Бэтмен, Супермен",
+    "powers": "верхчеловеческая сила искорость, выносливость, полет",
   },
-];
-const blackWidow = [
+
+
   {
-    name: "Черная вдова",
-    univers: "Marvel Comics",
-    alterago: "Наташа Романофф",
-    job: "супергероиня, шпионка",
-    friens: "Мстители",
-    powers:
+    "name": "Черная вдова",
+    "univers": "Marvel Comics",
+    "alterago": "Наташа Романофф",
+    "job": "супергероиня, шпионка",
+    "friens": "Мстители",
+    "powers":
       "пик человеческого физического потенциала, замедленное старение, знание многих языков",
   },
-];
-const Deadpool = [
+
+
   {
-    name: "Дэдпул",
-    univers: "Marvel Comics",
-    alterago: "Уэйд Уинстон Уилсон",
-    job: "антигерой, наемник",
-    friens: "частично Мстители, Человек-паук, Росомаха",
-    powers:
+    "name": "Дэдпул",
+    "univers": "Marvel Comics",
+    "alterago": "Уэйд Уинстон Уилсон",
+    "job": "антигерой, наемник",
+    "friens": "частично Мстители, Человек-паук, Росомаха",
+    "powers":
       "высокий болевой порог, регенерация и бессмертие, сверхчеловеческая иммунная система",
-  },
-];
+  }
+]`;
+
+document.addEventListener("DOMContentLoaded", function()  {
+     let cards=JSON.parse(superherous);
+     let cardscontent = "";
+     for (let card of cards) {
+        cardscontent+=`
+        <article class="card card--10">
+          <div class="card__info-hover"></div>
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+             <div class="card__img--hover"></div></a>
+                <div class="card__info">
+            <p class="card__category">${card.name}</p>
+            <p class="card__title">Вселенная: <strong>${card.univers}</strong></p>
+            <p class="card__title">Альтер эго: <strong>${card.alterago}</strong></p>
+            <p class="card__title">Род деятельности: <strong>${card.job}</strong></p>
+            <p class="card__title">Друзья: <strong>${card.friens}</strong></p>
+            <p class="card__title">Суперсилы: <strong>${card.powers}</strong></p>
+            <span class="card__by"> <a href="#" class="card__author" title="author">RATE</a></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+        </article>`;
+             }
+             document.getElementById('cardscontent').innerHTML = cardscontent;
+            });
